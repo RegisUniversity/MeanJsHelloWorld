259c16cf5c799246b48073f27ee8d26cbbb73467
cwdrunner <nitroxbill@msn.com>
2016-04-03 20:19:05 -0600

    Increase test time

--- test/pages.js
+++ test/pages.js
@@ -21,7 +21,9 @@ var assert = require('assert'),
 
 describe('/', function () {
   it('should return 200', function (done) {
+    
     http.get('http://localhost:8082', function (res) {
+        this.timeout(5000); 
       assert.equal(200, res.statusCode);
       done();
     });
